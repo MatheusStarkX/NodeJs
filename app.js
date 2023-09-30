ff5b797fd042f02879454032dd099a409b469e77
MatheusStarkX <matheusfelipemdeassis@gmail.com>
2023-09-29 21:20:51 -0300

    Section 6 completed (Node JS course)

--- app.js
+++ app.js
@@ -3,19 +3,23 @@ const path = require('path');
 const express = require('express');
 //const bodyParser = require('body-parser');
 
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+    //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -2,13 +2,28 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../Helper/path');
+//const rootDir = require('../Helper/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
+// router.get('/', (req, res, next) => {
+//     // console.log(adminData.products);
+//     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+//     // res.render('shop');
+// });
+
 router.get('/', (req, res, next) => {
-    // console.log('In another middleware!');
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    const products = adminData.products;
+    res.render('shop', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+    });
 });
+  
 
 module.exports = router;
