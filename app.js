3851e28897ecc5f3ef24c65589c43c82b7ac1e62
MatheusStarkX <matheusfelipemdeassis@gmail.com>
2023-10-09 17:54:00 -0300

    Section 7 completed (Node JS course)

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const path = require('path');
 const express = require('express');
 //const bodyParser = require('body-parser');
 
-const adminData = require('./routes/admin');
+const errorController = require('./controllers/error');
+
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
@@ -14,12 +16,9 @@ app.set('views', 'views');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
-    //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-});
+app.use(errorController.get404);
 
 app.listen(3000);
